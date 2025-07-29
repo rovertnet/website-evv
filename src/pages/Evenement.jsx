e252e9dd78151f072dc0f12ecc9e54a203670320
rovertnet <matundukabamba@gmail.com>
2025-07-29 22:39:23 +0100

    another commit

--- src/pages/Evenement.jsx
+++ src/pages/Evenement.jsx
@@ -7,7 +7,11 @@ import ReplaysSection from '../component/events/ReplaysSection';
 export default function Evenement() {
   return (
     <>
-      <HeroSlider />
+      <SectionBanner
+        title="📅 Événements à venir"
+        subtitle="Ne manquez pas nos conférences, séminaires, et rencontres communautaires."
+        image="https://cdn.pixabay.com/photo/2015/10/12/15/00/conference-984074_1280.jpg"
+      />
       <EventsSections />
       <ReplaysSection />
     </>
